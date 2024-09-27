ca02410f31123c7ac38938cf4e00b03c38f1dfd8
unknown <dorahuairesolis@gmail.com>
2024-09-26 19:55:43 -0500

    implementar la función completa con test completos

--- src/main.py
+++ src/main.py
@@ -9,39 +9,50 @@ def guess_number(player_assumptions, computer_assumptions):
     secret_number = number_random()
     #iniciamor con el jugador
     turn = "Jugador"
+    game_over = False
 
-    while True:
+    while not game_over:
         print(f"\n----Round:{turn}----")
         #Llama a la función de computer y player para obtener sus números
-        if turn == "Jugador":
-           chosen_number = number_player() 
-           player_assumptions.append(chosen_number)
-        else: 
-           chosen_number = number_computer()  
-           computer_assumptions.append(chosen_number)
-    
-        #Si el número es None, vuelve a intentarlo
-        if chosen_number is None:
-            continue
-        
-        #compara el número para ver si es el correcto
-        result = check_number(chosen_number, secret_number)
-        print(result)
-        
-        if chosen_number == secret_number:
+        try:
             if turn == "Jugador":
-               print(f"Estas fueron tus suposiciones: {player_assumptions}")
-               return "Jugador" 
+              chosen_number = number_player() 
+              if chosen_number is None:
+                 print(f"Número inválido")
+                 continue
+              player_assumptions.append(chosen_number)
             else: 
-               print(f"Estas fueron tus suposiciones: {computer_assumptions}")
-               return "Computadora" 
+              chosen_number = number_computer()  
+              computer_assumptions.append(chosen_number)
+    
+            #Si el número está fuera de rango
+            if not (1 <= chosen_number <= 100):
+               print(f"Número inválido, debe estar entre 1 y 100.")
+               continue
+        
+            #compara el número para ver si es el correcto
+            result = check_number(chosen_number, secret_number)
+            print(result)
+        
+            if chosen_number == secret_number:
+               game_over = True
+               if turn == "Jugador":
+                  print(f"Estas fueron tus suposiciones: {player_assumptions}")
+                  return "Jugador"
+               else: 
+                  print(f"Estas fueron tus suposiciones: {computer_assumptions}")
+                  return "Computadora" 
             
-        # si el turno es jugador, entonces cambia a computador
-        if turn == "Jugador":
-           turn = "Computadora" 
-        # de lo contrario si el turno es computador pasa a jugador
-        else: 
-           turn = "Jugador"
+            # si el turno es jugador, entonces cambia a computador
+            if turn == "Jugador":
+               turn = "Computadora" 
+            # de lo contrario si el turno es computador pasa a jugador
+            else: 
+               turn = "Jugador"
+     
+        except Exception as e:
+            print(f"Ocurrió un error inesperado: {str(e)}")
+            continue 
       
 #Definimos una función que realice ejecute un if que compare si el número del jugador y de la computadora es el correcto o no
 def check_number(chosen_number, secret_number):
@@ -65,11 +76,15 @@ def number_computer():
 def number_player():
     try:
         number = int(input("Escribe un número entero (entre 1 y 100): "))
-        return number
+        # Verificar el rango
+        if 1 <= number <= 100:  
+           return number
+        else:
+            print("\nEl número debe estar entre 1 y 100.")
+            return None       
     except ValueError:
         print("Por favor ingresa un número entero válido.")
         return None  #Retorna None si el input no es un número válido
-
 
 #Reiniciamos el juego, si el jugador quiere continuar
 def play_again(): 
@@ -77,24 +92,27 @@ def play_again():
 
     if restart_game.lower() == 'sí' or restart_game.lower() == 'si':
        print("¡Genial! ¡Vamos a jugar nuevamente!")
-       return start_game()
+       return True
     else:
        print("Gracias por jugar. ¡Hasta la próxima!")
-    
+       return False
 
 #Definimos la función que de iniciar el juego con la bienvenida, llama a la ejecución del juego y a volver a jugar
 def start_game():
     print("\n----¡Bienvenido a Guess the Number!---- \nTú y la computadora intentarán adivinar el número correcto")
+   
+    while True:
+         #definimos las listas 
+         player_assumptions = [] 
+         computer_assumptions = [] 
     
-    #definimos las listas 
-    player_assumptions = [] 
-    computer_assumptions = [] 
+         #iniciamos el juego
+         winner = guess_number(player_assumptions, computer_assumptions)
+         print(f"\nEl ganador es: {winner}")
     
-    #iniciamos el juego
-    guess_number(player_assumptions, computer_assumptions)
-    
-    #Una vez terminado, damos la opción de reiniciar el juego
-    return play_again() 
+         #Una vez terminado, damos la opción de reiniciar el juego
+         if not play_again():
+            break 
 
 if __name__ == '__main__':
    start_game()
--- test/test_game.py
+++ test/test_game.py
@@ -1,5 +1,5 @@
 import unittest
-from src.main import check_number, guess_number, start_game
+from src.main import check_number, guess_number, start_game, number_player
 from unittest.mock import patch, Mock
 
 mock = Mock()
@@ -21,6 +21,46 @@ class TestGuessTheNumber(unittest.TestCase):
     def test_check_number_equal(self):
         result = check_number(chosen_number = 5, secret_number = 5)
         self.assertEqual(result, "\n----¡Felicidades! ¡Adivinaste el número correcto!----")
+        
+    #number_player: el objetivo es recibir un número entero por parte del jugador
+    #no recibe parámetros y retorna nombre 
+    #la función está diseñada para retornar unicamente enteros, si recibe un string debe convertirlo a número
+    @patch('builtins.input')
+    def test_number_player(self, mock_input):
+        mock_input.side_effect = ["5"]
+        result = number_player()
+        self.assertEqual(result, 5)
+        
+    #si recibe un flotante, debe devolver none
+    #debe retornar none
+    @patch('builtins.input')
+    def test_number_player_float(self, mock_input):
+        mock_input.side_effect = ["3.34"]
+        result = number_player()
+        self.assertEqual(result, None)
+    
+    #si recibe un negativo
+    #debe retornar none
+    @patch('builtins.input')
+    def test_number_player_negative(self, mock_input):
+        mock_input.side_effect = ["-3"]
+        result = number_player()
+        self.assertEqual(result, None)
+    
+    #si recibe un número fuera del rango
+    #debe retornar none
+    @patch('builtins.input')
+    def test_number_player_out_range(self, mock_input):
+        mock_input.side_effect = ["101"]
+        result = number_player()
+        self.assertEqual(result, None)
+    
+    #si recibe un string
+    @patch('builtins.input')
+    def test_number_player_invalid_input(self, mock_input):
+        mock_input.side_effect = ["hola"]
+        result = number_player()
+        self.assertEqual(result, None)
     
     #guess_number: captura los números y valida los turnos - jugador
     #definimos sus dos parámetros player_assumptions y computer_assumptions
@@ -30,32 +70,29 @@ class TestGuessTheNumber(unittest.TestCase):
     def test_player_winner(self, mock_number_random, mock_number_player): 
         mock_number_random.return_value = 5
         mock_number_player.return_value = 5
-        result = guess_number(player_assumptions = [], computer_assumptions = [])
+        result = guess_number([],[])
         self.assertEqual(result, "Jugador")
     
-    #guess_number: captura los números y valida los turnos - jugador
+    #validamos si el jugador retorna None, el juego debe continuar
     @patch("src.main.number_player")
-    @patch("src.main.number_computer")
     @patch("src.main.number_random")
-    def test_computer_winner(self, mock_number_random, mock_number_computer, mock_number_player): 
-        mock_number_random.return_value = 5
-        mock_number_computer.return_value = 5
-        mock_number_player.return_value = 20
-        result = guess_number(player_assumptions = [], computer_assumptions = [])
-        self.assertEqual(result, "Computadora")
+    def test_player_winner(self, mock_number_random, mock_number_player): 
+        mock_number_random.return_value = 10
+        mock_number_player.side_effect = [None,10]
+        result = guess_number([],[])
+        self.assertEqual(result, "Jugador") 
         
-    #start_game: captura los números y valida los turnos - jugador
+    #start_game
+    #validamos si el reinicio del juego funciona
     @patch("src.main.guess_number")
     @patch("src.main.play_again")
-    def test_start_game(self, mock_play_again, mock_guess_number):
-        mock_guess_number.return_value = None
-        mock_play_again.return_value = "si"
-        result = start_game()
-        mock_guess_number.assert_called_once_with([], [])  
-        mock_play_again.assert_called()
-        
-        self.assertEqual(result, "si")
-    
+    def test_start_game_out_range(self, mock_play_again, mock_guess_number):
+        mock_play_again.return_value = False
+        mock_guess_number.return_value = "Jugador"
+        start_game()
+        mock_guess_number.assert_called_once_with([], [])
+        mock_play_again.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
